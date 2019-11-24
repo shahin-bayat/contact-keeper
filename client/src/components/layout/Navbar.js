f1620654ffc6d11187cc02ceefa3ac8337ffd333
Shahin Bayat <me@shahinbayat.ir>
2019-11-24 19:02:04 +0330

    all functionality added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,20 @@
 import React, { Fragment, useContext } from "react"
 import { Link } from "react-router-dom"
 import AuthContext from "./../../context/auth/authContext"
+import ContactContext from "./../../context/contact/contactContext"
 
 import PropTypes from "prop-types"
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext)
+  const contactContext = useContext(ContactContext)
+
   const { isAuthenticated, logout, user } = authContext
+  const { clearContacts } = contactContext
 
   const handleLogout = () => {
     logout()
+    clearContacts()
   }
 
   const authLinks = (
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,7 +4,6 @@ import Contacts from "./../contacts/Contacts"
 import ContactForm from "./../contacts/ContactForm"
 import ContactFilter from "./../contacts/ContactFilter"
 import AuthContext from "./../../context/auth/authContext"
-import AuthState from "../../context/auth/AuthState"
 
 const Home = () => {
   const authContext = useContext(AuthContext)
